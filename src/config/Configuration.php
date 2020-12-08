09fee9aa07f2e8f9979d3a129592a3f72e868e0c
Teddy Walter <teddywalter2016@gmail.com>
2020-12-08 23:11:58 +0000

    Update config

--- src/config/Configuration.php
+++ src/config/Configuration.php
@@ -2,7 +2,7 @@
     /*===ALL CONFIGURATIONS ======*/
     namespace config;
     class Configuration {
-        public static $key = "YOUR_KEY_HERE";
+        protected static $key = "YOUR_KEY_HERE";
         protected static $db_host = "localhost";
         protected static $db_user = "YOUR_USER_NAME";
         protected static $db_password = "YOUR_PASSWORD";
