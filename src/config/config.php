2f99240a0c856b9c4041426bba7776b45a09d4f6
Teddy Walter <teddywalter2016@gmail.com>
2020-12-08 20:20:55 +0000

    create config class

---
+++ src/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+    /*===ALL CONFIGURATIONS ======*/
+    class Configuration {
+        protected static $key = "YOUR_KEY_HERE";
+        protected static $db_host = "localhost";
+        protected static $db_user = "YOUR_USER_NAME";
+        protected static $db_password = "YOUR_PASSWORD";
+        protected static $db_name = "YOUR_DB_NAME";
+    }
+
+?>
