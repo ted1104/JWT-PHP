02afda22f89751438924c12eb6242c25f7577578
Teddy Walter <teddywalter2016@gmail.com>
2020-12-08 22:57:38 +0000

    update configuration

--- src/config/Configuration.php
+++ src/config/Configuration.php
@@ -1,12 +1,13 @@
 <?php 
     /*===ALL CONFIGURATIONS ======*/
-    namespace Configuration;
+    namespace config;
     class Configuration {
-        protected static $key = "YOUR_KEY_HERE";
+        public static $key = "YOUR_KEY_HERE";
         protected static $db_host = "localhost";
         protected static $db_user = "YOUR_USER_NAME";
         protected static $db_password = "YOUR_PASSWORD";
         protected static $db_name = "YOUR_DB_NAME";
+        
     }
 
 ?>
